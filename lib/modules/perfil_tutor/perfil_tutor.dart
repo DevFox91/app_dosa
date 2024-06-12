f056a799b8d46a6e2f0b1b7bc75aaf6317f79ff8
DevFox91 <devfox91@gmail.com>
2024-06-12 17:31:55 +0200

    Update .gitignore to exclude credentials file

--- lib/modules/perfil_tutor/perfil_tutor.dart
+++ lib/modules/perfil_tutor/perfil_tutor.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:app_dosa/services/api_receiver.dart';
 import 'package:app_dosa/services/api_sender.dart';
 
+// Definición de la clase StatefulWidget para la pantalla de perfil del tutor
 class PerfilTutorScreen extends StatefulWidget {
   const PerfilTutorScreen({super.key});
 
@@ -9,7 +10,9 @@ class PerfilTutorScreen extends StatefulWidget {
   _PerfilTutorScreenState createState() => _PerfilTutorScreenState();
 }
 
+// Estado de la pantalla de perfil del tutor
 class _PerfilTutorScreenState extends State<PerfilTutorScreen> {
+  // Controladores de texto para cada campo del formulario
   final TextEditingController _nombreController = TextEditingController();
   final TextEditingController _apellido1Controller = TextEditingController();
   final TextEditingController _apellido2Controller = TextEditingController();
@@ -27,22 +30,28 @@ class _PerfilTutorScreenState extends State<PerfilTutorScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Creación de una instancia de ApiReceiver, que utiliza ApiSender
     final ApiReceiver apiReceiver = ApiReceiver(ApiSender());
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Perfil del Tutor'),
       ),
+      // Uso de FutureBuilder para manejar la obtención de datos del usuario
       body: FutureBuilder<Map<String, String>>(
         future: apiReceiver.getUserData('1'), // ID del usuario
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
+            // Muestra un indicador de carga mientras se esperan los datos
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
+            // Muestra un mensaje de error si ocurre un problema al obtener los datos
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            // Muestra un mensaje si no hay datos disponibles
             return const Center(child: Text('No hay datos disponibles'));
           } else {
+            // Si los datos se obtienen correctamente, se rellenan los controladores de texto
             final userData = snapshot.data!;
             _nombreController.text = userData['nombre'] ?? '';
             _apellido1Controller.text = userData['apellido1'] ?? '';
@@ -59,6 +68,7 @@ class _PerfilTutorScreenState extends State<PerfilTutorScreen> {
             _provinciaController.text = userData['provincia'] ?? '';
             _codigoPostalController.text = userData['cpostal'] ?? '';
 
+            // Construcción de la interfaz de usuario
             return Padding(
               padding: const EdgeInsets.all(16.0),
               child: SingleChildScrollView(
@@ -70,6 +80,7 @@ class _PerfilTutorScreenState extends State<PerfilTutorScreen> {
                       style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
                     const SizedBox(height: 8.0),
+                    // Uso de Wrap para organizar los campos de texto
                     Wrap(
                       spacing: 16.0, // Espacio horizontal entre los campos
                       runSpacing: 8.0, // Espacio vertical entre las filas de campos
@@ -121,6 +132,7 @@ class _PerfilTutorScreenState extends State<PerfilTutorScreen> {
     );
   }
 
+  // Método para construir un campo de texto con etiqueta
   Widget _buildTextField(String labelText, TextEditingController controller) {
     return SizedBox(
       width: 300, // Ajusta el ancho de los campos de texto
@@ -146,10 +158,11 @@ class _PerfilTutorScreenState extends State<PerfilTutorScreen> {
   // Función para guardar los datos del usuario
   Future<void> _saveUserData(BuildContext context, ApiSender apiSender) async {
     print('Guardando datos del usuario...');
+    // Creación de un mapa con los nuevos datos del usuario
     final Map<String, String> newData = {
       'nombre': _nombreController.text,
       'apellido1': _apellido1Controller.text,
-            'apellido2': _apellido2Controller.text,
+      'apellido2': _apellido2Controller.text,
       'tipodoc': _tipoDocController.text,
       'numdoc': _numDocController.text,
       'sexo': _sexoController.text,
@@ -164,12 +177,15 @@ class _PerfilTutorScreenState extends State<PerfilTutorScreen> {
     };
 
     try {
+      // Llamada al método para actualizar los datos del usuario
       await apiSender.updateUserData('1', newData); // ID del usuario
       print('Datos del usuario actualizados con éxito.');
+      // Muestra una notificación de éxito
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Datos actualizados correctamente')),
       );
     } catch (e) {
+      // Manejo de errores al actualizar los datos del usuario
       print('Error al actualizar los datos del usuario: $e');
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Error al actualizar los datos')),
@@ -177,5 +193,3 @@ class _PerfilTutorScreenState extends State<PerfilTutorScreen> {
     }
   }
 }
-
-     
